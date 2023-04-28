f6351826c979282b24906db1858aa245d1bc8e5b
Harsh Tandiya <harsh.tandiya@gmail.com>
2023-04-28 11:00:48 +0530

    theme color added

---
+++ lib/src/utils/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class UAppTheme {
+  static const _baseBlue = 0xFF30A5BD;
+
+  static ThemeData baseTheme = ThemeData(
+    // brightness: Brightness.dark,
+    primarySwatch: const MaterialColor(_baseBlue, <int, Color>{
+      50: Color(0xFFEAF6F8),
+      100: Color(0xFFBFE3EB),
+      200: Color(0xFFA0D6E1),
+      300: Color(0xFF74C3D3),
+      400: Color(0xFF59B7CA),
+      500: Color(_baseBlue),
+      600: Color(0xFF2C96AC),
+      700: Color(0xFF227586),
+      800: Color(0xFF1A5B68),
+      900: Color(0xFF14454F),
+    }),
+    scaffoldBackgroundColor: const Color.fromARGB(255, 36, 43, 42),
+    textTheme: GoogleFonts.poppinsTextTheme(),
+  );
+}
